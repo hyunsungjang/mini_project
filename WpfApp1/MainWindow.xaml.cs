7deeb4b7349956b93682ee0bc3b800779de06a6f
Hyunsung Jang <34292150+hyunsungjang@users.noreply.github.com>
2023-04-14 14:55:25 +0900

    Update MainWindow.xaml.cs

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // tb1 에 문자열 출력
+            // tb1 에 문자열 출력b
             tb1.Text = "Hello World!";
         }
     }
